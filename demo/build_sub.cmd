749bc83b5bdaf7f6cffed386ac1afc259565f379
sundayliu <martinkro@aliyun.com>
2015-03-26 20:16:03 +0800

    sub test

--- demo/build_sub.cmd
+++ demo/build_sub.cmd
@@ -6,7 +6,15 @@ clang++.exe -v -gcc-toolchain d:/android-ndk-r10d/toolchains/arm-linux-androidea
 -Id:/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/libs/armeabi/include ^
 -Id:/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/backward ^
 -Id:/android-ndk-r10d/platforms/android-3/arch-arm/usr/include ^
--fexceptions -frtti -Os -emit-llvm -mllvm -sub ^
+-fexceptions -frtti -Os -emit-llvm  ^
 -o sub.ll -S -target armv5te-none-linux-androideabi sub.cpp 
+
+clang++.exe -v -gcc-toolchain d:/android-ndk-r10d/toolchains/arm-linux-androideabi-4.8/prebuilt/windows ^
+-Id:/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include ^
+-Id:/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/libs/armeabi/include ^
+-Id:/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/backward ^
+-Id:/android-ndk-r10d/platforms/android-3/arch-arm/usr/include ^
+-fexceptions -frtti -Os -emit-llvm -mllvm -sub -mllvm -bcf -mllvm -fla  ^
+-o sub_o.ll -S -target armv5te-none-linux-androideabi sub.cpp 
 
 pause
--- demo/sub.cpp
+++ demo/sub.cpp
@@ -2,10 +2,12 @@
  * instruction substitution test
  */
 
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
 
 #include <stdio.h>
+//#include <vector>
+//using namespace std;
 // +
 int t_addition(int a, int b )
 {
@@ -41,9 +43,25 @@ int t_XOR(int a, int b)
     c = a ^ b;
     return c;
 }
+
+void test_sub()
+{
+    int a = 4;
+    int b = 1100;
+    int c = 4;
+    
+    c = 4 + a + b - c;
+    printf("c:%d\n", c);
+    
+    c = c / a;
+    printf("c:%d\n",c);
+    
+    a = a + c + b;
+    printf("a:%d\n",a);
+}
 int main(int argc, char* argv[])
 {
-    cout << "+++ Instruction Substitution Test +++" << endl;
+    printf("+++ Instruction Substitution Test +++\n");
     int a = 0x100;
     int b = 0x200;
     
@@ -53,5 +71,20 @@ int main(int argc, char* argv[])
     printf("%d | %d = %d\n", a, b, t_OR(a,b));
     printf("%d ^ %d = %d\n", a, b, t_XOR(a,b));
     
+    test_sub();
+
+    /*
+    vector<int> vec_a;
+    for (int i = 0; i < 10; i++)
+    {
+        vec_a.push_back(i);
+    }
+    
+    vector<int>::iterator it = vec_a.begin();
+    while (it != vec_a.end())
+    {
+        printf("%d\n", *it);
+    }
+    */
     return 0;
 }
